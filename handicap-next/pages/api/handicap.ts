c8768161d87ef63b691782b705d6833af99d4cd8
mjsteele2099 <mjsteele2099@gmail.com>
2025-07-14 21:58:15 -0400

    9 and 18 hole selection

--- handicap-next/pages/api/handicap.ts
+++ handicap-next/pages/api/handicap.ts
@@ -17,6 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       id,
       gross_score, 
       date_played,
+      holes_played,
       courses!inner (
         id,
         name,
